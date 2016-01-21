831443accc262e54beabaaca75bc127de96540cd
Samuel Colvin <s@muelcolvin.com>
2016-01-21 18:38:27 +0000

    using em2-test-utils for test plugins

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ setup(
     zip_safe=True,
     install_requires=[
         'pytz>=2015.7',
-        'aiohttp==0.20.0',
-        'cchardet==1.0.0',
-        'Cerberus==0.9.2',
+        'aiohttp>=0.20.0',
+        'cchardet>=1.0.0',
+        'Cerberus>=0.9.2',
     ]
 )
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,40 +1,9 @@
 import pytest
-import asyncio
-import gc
 
 from em2.base import Controller
 from tests.fixture_classes import SimpleDataStore, NullPropagator
 
-
-def pytest_pycollect_makeitem(collector, name, obj):
-    if collector.funcnamefilter(name) and asyncio.iscoroutinefunction(obj):
-        return list(collector._genfunctions(name, obj))
-
-
-def pytest_pyfunc_call(pyfuncitem):
-    """
-    Run coroutines in an event loop instead of a normal function call.
-    """
-    if asyncio.iscoroutinefunction(pyfuncitem.function):
-        funcargs = pyfuncitem.funcargs
-        loop = funcargs.get('loop') or asyncio.get_event_loop()
-        testargs = {arg: funcargs[arg] for arg in pyfuncitem._fixtureinfo.argnames}
-        loop.run_until_complete(loop.create_task(pyfuncitem.obj(**testargs)))
-        return True
-
-
-@pytest.yield_fixture
-def loop():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(None)
-
-    yield loop
-
-    loop.stop()
-    loop.run_forever()
-    loop.close()
-    gc.collect()
-    asyncio.set_event_loop(None)
+pytest_plugins = 'em2_tests.asyncio'
 
 
 @pytest.fixture
