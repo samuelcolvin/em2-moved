daeeba49e9588dd58790746ba5c468b0d1d136c1
Samuel Colvin <s@muelcolvin.com>
2016-01-25 13:58:22 +0000

    setup.cfg creation

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-py.test --cov=em2 tests/
+py.test
 pytest=$?
 echo "pytest exit code: ${pytest}"
-flake8 --max-line-length 120 em2/ tests/
+flake8 em2/ tests/
 flake=$?
 echo "flake exit code:  ${flake}"
 exit $((${flake} + ${pytest}))
